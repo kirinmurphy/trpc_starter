2b4928165217133c8795f5118f4dce9fb980aec4
KIRIN MURPHY <kirinmurphy@gmail.com>
2024-10-01 16:31:52 -0600

    more endpoints

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,41 @@ import { TRPCLoader } from './components/TRPCLoader';
 import { trpcService } from './utils/trpc'
 
 function App() {
-  
-  const issAstrosResponse = trpcService.getAstronautsOnISS.useQuery();
+  const nasaPicOfTheDay = trpcService.getNasaPicOfTheDay.useQuery();
+  const marsRoverPics = trpcService.getMarsRoverPics.useQuery();
+  const issAstrosQuery = trpcService.getAstronautsOnISS.useQuery();
+  const issLocationQuery = trpcService.getISSLocation.useQuery(undefined, {
+    refetchInterval: 30 * 60 * 1000,
+    refetchIntervalInBackground: false,
+  });
+
+  console.log('======= nasaPicOfTheDay', nasaPicOfTheDay?.data);
 
   return (
     <>
-      <TRPCLoader query={issAstrosResponse}>
-        {(issAstrosData) => <>
-          <header><h2>Current ISS Astros</h2></header>
-          {issAstrosData.people.map(({ name }) => <li key={name}>{name}</li>)}
-        </>}
-      </TRPCLoader>
+      <div>
+        <TRPCLoader query={marsRoverPics}>
+          {({ photos }) => <>
+            <div><img src={photos[0].img_src} /></div>
+          </>}
+        </TRPCLoader>
+        <TRPCLoader query={issLocationQuery}>
+          {({ iss_position }) => <>
+            <div> 
+              <strong>
+                <a href="https://www.nasa.gov/international-space-station/" target="_blank">ISS</a>
+                </strong>{" "}
+               {iss_position.latitude} / {iss_position.longitude}
+            </div>
+          </>}
+        </TRPCLoader>
+        <TRPCLoader query={issAstrosQuery}>
+          {({ people }) => <>
+            {people.map(({ name, craft }) => <li key={name}>{name} ({craft})</li>)}
+          </>}
+        </TRPCLoader>
+
+      </div>
     </>
   )
 }
--- src/server/server.ts
+++ src/server/server.ts
@@ -3,6 +3,30 @@ import { createHTTPServer } from "@trpc/server/adapters/standalone";
 import cors from 'cors';
 import { z } from "zod";
 
+const MarsRoverPicsSchema = z.object({
+  photos: z.array(
+    z.object({
+      id: z.number(),
+      sol: z.number(),
+      camaera: z.any(),
+      img_src: z.string()
+    })
+  )
+});
+
+type MarsRoverPicsSchemaResponse = z.infer<typeof MarsRoverPicsSchema>;
+
+const ISSLocationSchema = z.object({
+  message: z.string(),
+  timestamp: z.number(),
+  iss_position: z.object({
+    latitude: z.string(),
+    longitude: z.string()
+  })
+});
+
+type ISSLocationSchemaResponse = z.infer<typeof ISSLocationSchema>;
+
 const AstroResponseSchema = z.object({
   message: z.string(),
   number: z.number(),
@@ -16,15 +40,39 @@ const AstroResponseSchema = z.object({
 
 type AstroResponse = z.infer<typeof AstroResponseSchema>;
 
+const nasaApiKey = process.env.NASA_API_KEY;
+console.log('nasaApiKey', nasaApiKey);
+
 export const appRouter = router({
+  getMarsRoverPics: publicProcedure
+    .query(async (): Promise<MarsRoverPicsSchemaResponse> => {
+      const url  = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${nasaApiKey}&page=1`;
+      const response = await fetch(url);
+      const data = await response.json();
+      return MarsRoverPicsSchema.parse(data);
+    }),
+  getNasaPicOfTheDay: publicProcedure
+    .query(async (): Promise<unknown> => {
+      const url = `https://api.nasa.gov/planetary/apod?api_key=${nasaApiKey}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      return data;
+    }),
   getAstronautsOnISS: publicProcedure
     .output(AstroResponseSchema)
     .query(async (): Promise<AstroResponse> => {   
       const response = await fetch(`http://api.open-notify.org/astros.json`);
       const data = await response.json();
-      console.log('DATAAAAAA', data);
       return AstroResponseSchema.parse(data);
     }),
+  getISSLocation: publicProcedure
+    .output(ISSLocationSchema)
+    .query(async (): Promise<ISSLocationSchemaResponse> => {
+      const response = await fetch(`http://api.open-notify.org/iss-now.json`);
+      const data = await response.json();
+      console.log('((((((((((((((((())))))))))))))))))))) DATA', data);
+      return data;
+    })
 });
 
 const server = createHTTPServer({
