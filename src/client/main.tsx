20f674ea27fb8bb16e5fae5e2bd03b97a2fefabd
KIRIN MURPHY <kirinmurphy@gmail.com>
2024-10-16 15:17:57 -0700

    added tailwind

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
+import './assets/index.css';
 import { trpcReactClient, trpcService } from '../utils/trpc.ts'
 import { createRouter, RouterProvider } from '@tanstack/react-router'
 import { routeTree } from './routing/routeTree.tsx'
